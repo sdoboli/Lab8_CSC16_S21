c6b4e243ca41fbfd223199e919b750a056cec164
Simona Doboli <simona.doboli@hofstra.edu>
2020-08-19 22:35:55 -0400

    Delete helloWorld.cpp

--- helloWorld.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream> // input output library: cin, cout, <<, >>
-using namespace std; // use libraries from namespace std
-
-int main()
-{
-	cout << "Hello CSC16 Fall 2020 class ! " << endl; 
-}
