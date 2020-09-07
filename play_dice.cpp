e55fcb5ded39b9e69e765f5879265ba2da0e2f69
Simona Doboli <simona.doboli@hofstra.edu>
2020-09-07 15:22:20 -0400

    Add files via upload

---
+++ play_dice.cpp
@@ -0,0 +1,20 @@
+/* Suppose we roll a single die, repeatedly if we like, and sum the faces. We can stop at any point, and the sum becomes our score; however, if we exceed 10, our score is zero. Play this game against the computer. What would be a good strategy for the computer */
+/* Add your name here */
+    
+#include <iostream>
+#include <cstdlib>  // contains the rand() and srand() function 
+#include <ctime>    // time() function
+using namespace std;
+
+
+int main()
+{
+    srand(time(0)); // initialize random seed generator to current time: 
+					// needed to generate a different sequence of random numbers
+					// each time you run your program
+
+    // Add your code to play the dice game here
+
+}
+
+
