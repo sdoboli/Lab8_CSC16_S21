0439b18806a8550ae921181575279da2e54b0322
Simona Doboli <simona.doboli@hofstra.edu>
2020-09-29 07:56:25 -0400

    Update header.h

--- header.h
+++ header.h
@@ -1,17 +1,14 @@
 // THIS IS THE HEADER FILE 
 // Contains all function declarations
-#ifndef SEARCH_SORT
-#define SEARCH_SORT
+#include <string>
+using namespace std;
 
-int  max_array(int [], int );
-int min_array(int [], int);
-void display_array(int [], int );
-int init_array(int [], int ,string);
-void swap(int &, int &);
-void sort_bubble(int [], int);
-void sort_insert(int [], int);
-int  linear_search(int [], int, int);
-int  binary_search(int [], int, int);
-bool is_sorted(int [], int, bool ascending = true);
+#ifndef STRINGS
+#define STRINGS
+
+bool unique_char(string);
+bool is_permutation(string, string);
+string add_one(string);
+bool one_edit(string, string);
 
 #endif
