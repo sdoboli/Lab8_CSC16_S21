977371ddd81a3bca3f2ab2634dda2f4593b416cb
Simona Doboli <simona.doboli@hofstra.edu>
2020-10-13 07:51:00 -0400

    Create Team.h

---
+++ Team/Team.h
@@ -0,0 +1,24 @@
+sing namespace std;
+
+#ifndef TEAM
+#define TEAM
+class Team
+{
+    private: // declare private data members
+        string name;  
+        int num_players;  // number of players
+        int num_wins;     // number of wins
+        int num_losses;   // number of losses
+
+    public: // declare function members
+        Team(); // default constructor
+        Team(string new_name, int new_players, int new_wins = 0, int new_losses = 0); // specific constructor
+        void setTeam(string other_name, int other_players, int other_wins, int other_losses); 
+
+        void display() const; // accessor function 
+	    void addWin();  
+	    void addLoss(); 
+	    bool isBetter(const Team &other) const; // accessor function 
+	    int getPoints() const; // accessor function
+};
+#endif
